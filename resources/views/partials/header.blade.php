8ef995db97798e2c8f4577bd58a9e3a44d597ed9
emamoldo <moldovanemanuel92.em@gmail.com>
2024-05-17 11:23:30 +0200

    Add the img folder and add the logo in the header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,10 @@
 <headder>
-    <h1>
-        Header Here
-    </h1>
+
+    <div class="d-flex justify-content-center align-items-center g-5">
+        <div>
+            <img class="logo" src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+        </div>
+        <nav class="links nav justify-content-center">
+        </nav>
+    </div>
 </headder>
